9956a4ef7516221b529b6a73835386697aae426d
memalloc <mhsplay228@gmail.com>
2025-08-01 19:04:09 +1000

    add imgui impl

---
+++ src/includes.hpp
@@ -0,0 +1,13 @@
+// windows / stl includes.
+#include <Windows.h>
+#include <WinUser.h>
+#include <dwmapi.h> // DwmExtendFrameIntoClientArea
+
+
+
+// third-party
+#include "../third-party/imgui/imgui.h"
+#include "../third-party/imgui/impl/imgui_impl_win32.h"
+#include "../third-party/imgui/impl/imgui_impl_dx11.h"
+#include <d3d11.h>
+#pragma comment(lib, "d3d11.lib")
